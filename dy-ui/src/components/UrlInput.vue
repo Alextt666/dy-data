c8a04475da9a7d53b2aca78248f8097a8e7681af
Alex Zhang <alexzhangyaotian@gmail.com>
2023-02-10 01:04:47 +0800

    add:引入EL组件库并增加基本逻辑

--- dy-ui/src/components/UrlInput.vue
+++ dy-ui/src/components/UrlInput.vue
@@ -1,32 +1,42 @@
 <template>
-  <div class="input-top" style="display: flex;">
-    <el-input
-    v-model="input"
-    type="password"
-    placeholder="Please input password"
-    show-password
-  />
+  <div class="input-top" >
+    <div class="input-wrapper">
+      <el-input
+        v-model="input"
+        placeholder="Please input the url "
+      />
+    </div>
 
-   <el-button style="display: inline-block;">确认</el-button>
+    <el-button style="width:80px" type="primary" @click="handleAdd">添 加</el-button>
   </div>
   <div>
     <ul>
-      <li>111</li>
-      <li>111</li>
-      <li>111</li>
-      <li>111</li>
-      <li>111</li>
+      <li v-for="(item,index) of inputArr" :key="index">{{ item }}</li>
     </ul>
   </div>
 </template>
 <script setup>
-import { ref } from 'vue';
-const input = ref('')
+import { ref ,reactive} from 'vue';
+const input = ref('');
+const inputArr = reactive([]);
+function handleAdd(){
+    if(input.value){
+        inputArr.push(input.value);
+        input.value = ''
+    }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .input-top {
   margin: 3rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.input-wrapper{
+    width: 40rem;
+    margin: 0 1.5rem;
 }
 </style>
