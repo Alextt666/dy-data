bdc130e994e295bd955ab44634d7158348dc0373
Alex Zhang <alexzhangyaotian@gmail.com>
2023-02-10 00:43:57 +0800

    add:front-end模块

---
+++ dy-ui/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld msg="Welcome to Your Data-Analyse"/>
+  <UrlInput/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import UrlInput from './components/UrlInput.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    UrlInput
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ dy-ui/src/components/UrlInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="input-top" style="display: flex;">
+    <el-input
+    v-model="input"
+    type="password"
+    placeholder="Please input password"
+    show-password
+  />
+
+   <el-button style="display: inline-block;">确认</el-button>
+  </div>
+  <div>
+    <ul>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+    </ul>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+const input = ref('')
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.input-top {
+  margin: 3rem 0;
+}
+</style>
